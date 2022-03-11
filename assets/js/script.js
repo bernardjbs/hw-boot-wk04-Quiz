2d33fdc7ffe6092da61ea99d6e0ed19c673c5703
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-03-12 03:52:38 +0800

    Added content to index.html. Added css styles to style.css. Removed time left from localstorage. Implemented highscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,38 +11,37 @@ const question = document.querySelector("#qH2");
 const startBtn = document.querySelector("#startBtn");
 const initialInput = document.querySelector("#initial-input");
 const timeEl = document.querySelector("#time");
+const highscoreSection = document.querySelector("#highscore-section");
+let highscoreUl = document.querySelector("#highscore-ul");
 
 // Variables.
 let qIdx = 0;
 let score = 0;
-let highScore = 0;
 let secondsLeft = 100;
+let timerInterval = 0;
+let highScoresLi = [];
 
 // Constants
 const ansLen = questions[qIdx].answers.length;
-const sectionToDisplay = [startPage, quiz, ansResultMsg, initials];
+const sectionToDisplay = [startPage, quiz, ansResultMsg, initials, highscoreSection];
+
+fillQnA(qIdx);
 
 // Starts the quiz when the start quiz button is pressed.
-fillQnA(qIdx);
 function startQuiz() {
-    renderQuiz();
-    if(secondsLeft < 0) {
-        localStorage.clear();
-    }
+    renderHighScores()
+    fillQnA(qIdx);
     startPage.dataset.visible = "false";
     quiz.dataset.visible = "true";
     startBtn.addEventListener("click", toDisplay());
+    flexSubContainer(quiz);
     timer();
 }
 
 // Will save quiz when window or tab is closed
 window.addEventListener("unload", function (event) {
-    if (secondsLeft > 0) {
-        saveQuiz();
-    }
+    //save local data
 })
-
-
 
 function checkAns(clicked_id) {
     document.getElementById("ans-result-msg").style.opacity = "100"
@@ -51,10 +50,12 @@ function checkAns(clicked_id) {
     if (qIdx < qLen - 1) {
         // Validating the answers. 
         if (clicked_id == questions[qIdx].answer) {
+            ansResultMsg.setAttribute("class", "ans-right");
             ansResultMsg.textContent = "The answer is CORRECT";
             score = score + 10;
         }
         else {
+            ansResultMsg.setAttribute("class", "ans-wrong");
             ansResultMsg.textContent = "The answer is WRONG";
             secondsLeft = secondsLeft - 20;
         }
@@ -66,59 +67,88 @@ function checkAns(clicked_id) {
     }
     else if (qIdx === qLen - 1) {
         log("time to input initials and show the results");
+        clearInterval(timerInterval);
         // Hide answers section and show initials section.            
         quiz.dataset.visible = "false";
         initials.dataset.visible = "true";
         toDisplay();
-
-        // Clear the local storage
-        localStorage.clear();
-        log("local storage 2: " + localStorage.quizInfo);
     }
     else {
         log("there has been an error");
     }
-    saveQuiz();
-}
-
-function saveQuiz() {
-    let quizInfo = {
-        qIdx: qIdx,
-        secondsLeft: secondsLeft,
-        initial: initialInput.value,
-        score: score,
-    }
-    // save quizInfo obj in local storage
-    localStorage.setItem("quizInfo", JSON.stringify(quizInfo));
-}
-
-function renderQuiz() {
-    let lastQuiz = JSON.parse(localStorage.getItem("quizInfo"));
-    if (lastQuiz != null) {
-        secondsLeft = lastQuiz.secondsLeft;
-        log("seconds left: " + lastQuiz.secondsLeft);
-        fillQnA(lastQuiz.qIdx);
-    }
 }
 
 function timer() {
-    let timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         log("i am here");
         secondsLeft--;
         timeEl.innerHTML = " - " + secondsLeft + " seconds left";
 
+        log("qIdx: " + qIdx + " - questions len: " + questions.length);
+        if (qIdx === (questions.length)) {
+            window.alert("Clearing interval");
+            clearInterval(timerInterval);
+        }
         if (secondsLeft === 0 || secondsLeft < 0) {
             // Stops execution of action at set interval.
             clearInterval(timerInterval);
             // Hide answers section and show initials section.            
             quiz.dataset.visible = "false";
             initials.dataset.visible = "true";
+            //saveHighScores();
             toDisplay();
-            // Clear the local storage
-            localStorage.clear()
-            log("local storage 1: " + localStorage.quizInfo);
         }
     }, 1000);
+}
+
+function saveHighScores() {
+    let highScores = {
+        games: []
+    }
+
+    let highScoresParsed = JSON.parse(localStorage.getItem("highScores"));
+
+    let game = {
+        name: initialInput.value,
+        score: score
+    }
+
+    if (highScoresParsed != null) {
+        alert("games array is not empty... push and assign highscoresArr = highScoresParsed")
+        highScoresParsed.games.push(game);
+        highScores = highScoresParsed;
+    }
+    else {
+        alert("games array is empty... push highScore")
+        highScores.games.push(game);
+    }
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    initials.dataset.visible="false";
+    highscoreSection.dataset.visible="true";
+    displayHighscores(highScores);
+    toDisplay();
+}
+
+function displayHighscores(highScores) {
+    let hs = highScores;
+
+    // alert("game: " + hs.games[0].score);
+    // For each highscore in highscores, create an li element and append it to ul
+    for(i=0;i<hs.games.length;i++) {
+        let newLi = document.createElement("li");
+        newLi.textContent = "Name: " + hs.games[i].name + " - Score: " + hs.games[i].score;
+        highscoreUl.appendChild(newLi);
+        // alert("looping in the hs array");        
+    }
+
+}
+
+function renderHighScores() {
+    let highScores = JSON.parse(localStorage.getItem("highScores"));
+    if (highScores != null) {
+        log("highScores lenght: " + highScores.games.length);
+    }
 }
 
 function fillQnA(qIdx) {
@@ -127,6 +157,7 @@ function fillQnA(qIdx) {
     for (i = 0; i < ansLen; i++) {
         document.getElementById(i + 1).textContent = questions[qIdx].answers[i];
     }
+    // flexSubContainer(quiz);
 }
 
 function toDisplay() {
@@ -134,11 +165,22 @@ function toDisplay() {
         if (sectionToDisplay[i].dataset.visible === "false") {
             sectionToDisplay[i].style.display = "none";
         } else {
-            sectionToDisplay[i].style.display = "block";
+            sectionToDisplay[i].style.display = "flex";
         }
     }
 }
 
 function fadeoutMsg() {
     document.getElementById("ans-result-msg").style.opacity = "0";
+}
+
+function clearHs() {
+    alert("i am here");
+    window.localStorage.removeItem("highScores");
+    document.querySelector("#high-score-list").style.display = "none";
+    document.querySelector("#clear-btn").style.display = "none";
+    document.querySelector("#highscore-h2").textContent = "Highscores Cleared";
+    let clearText = document.createElement("p");
+    clearText.textContent = "Press 'Play Again' if you wish to play again";
+    highscoreSection.appendChild(clearText);
 }
